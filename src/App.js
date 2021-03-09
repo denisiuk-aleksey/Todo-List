0c9527f5d97118cd8f49365207b46e59cd52d7f0
denisiuk-aleksey <denisiuk.aleksei@gmail.com>
2021-03-09 15:04:51 +0200

    Add basiс markup and correct styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import TodoHeader from './components/TodoListHeader';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <TodoHeader />
+      {/* {<TodoInput />
+      <TodoTaskList />} */}
     </div>
   );
 }
