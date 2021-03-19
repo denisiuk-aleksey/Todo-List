92cdafd725396d17c395d7e861baeb1d5b5186d6
denisiuk-aleksey <denisiuk.aleksei@gmail.com>
2021-03-19 22:16:45 +0200

    Add list item and styles for it

--- src/components/TaskList/ListItem/index.jsx
+++ src/components/TaskList/ListItem/index.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import styles from './ListItem.module.scss'
+import cx from 'classnames'
+import Icon from '@mdi/react';
+import { mdiTrashCanOutline } from '@mdi/js';
 
 const ListItem = props => {
-  const { itemBody } = props
-  return <li> {itemBody} </li>
+  const { body, id, isDone, deleteTask } = props
+  const [isTaskDone, setIsTaskDone] = useState(isDone)
+
+  const onDeleteTask = () => {
+    deleteTask(id)
+  }
+
+  const className = cx(styles.taskDefalt, {
+    [styles.taskDone]: isTaskDone,
+    [styles.taskCancel]: !isTaskDone
+  })
+
+  return (
+    <div onClick={() => setIsTaskDone(!isTaskDone)} className={styles.container}>
+      <li className={className}>
+        <span>{body}</span>
+      </li>
+      <Icon path={mdiTrashCanOutline} size={1} color="white" onClick={onDeleteTask}/>
+    </div>
+  )
+}
+
+ListItem.propTypes = {
+  body: PropTypes.string.isRequired,
+  isDone: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired,
+  onDeleteTask: PropTypes.func,
+  switchTask: PropTypes.func
 }
 
 export default ListItem
