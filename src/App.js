36cfc070c525083d02b6f4bcee75bf192655f1e7
denisiuk-aleksey <denisiuk.aleksei@gmail.com>
2021-03-19 22:11:34 +0200

    Refactor app structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { useState } from 'react'
 import TodoHeader from './components/TodoHeader'
 import TodoInput from './components/TodoInput'
+import TaskList from './components/TaskList'
 import './App.css'
 
 const App = props => {
+  const [todoArray, setTodoArray] = useState([
+    {
+      id: Date.now(),
+      body: 'Your task may be here...',
+      isDone: true,
+      createdAt: new Date()
+    }
+  ])
+
+  const addTask = values => {
+    const newTodo = {
+      body: values.todo,
+      id: Date.now(),
+      isDone: true,
+    };
+    setTodoArray([...todoArray, newTodo]);
+  };
+
+  const deleteTask = id => {
+    const filteredTodos = todoArray.filter(
+      currentTask => currentTask.id !== id
+    );
+    setTodoArray(filteredTodos);
+  };
+  
   return (
     <div className='container'>
       <TodoHeader />
-       <TodoInput />
-    {/* {  <TodoTaskList />} */}
+      <TodoInput onSubmit={addTask} />
+      <TaskList todoArray={todoArray} deleteTask={deleteTask}/>
     </div>
   )
 }
