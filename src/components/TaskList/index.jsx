56d875ef18035bbe3b99d16b9a1fc61283b86ab5
denisiuk-aleksey <denisiuk.aleksei@gmail.com>
2021-03-19 22:18:03 +0200

    Refactor TaskList

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ListItem from './ListItem'
 
 const TaskList = props => {
-  const { todoArray } = props
+  const { todoArray, deleteTask } = props
+
   return (
     <ul>
-      {todoArray.map(item => {
-        return <ListItem key={item.id} />
+      {todoArray.map(task => {
+        return <ListItem {...task} key={task.id} deleteTask={deleteTask}/>
       })}
     </ul>
   )
