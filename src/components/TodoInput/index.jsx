7161b879db3a4ed8cbe61feec1cd9147b0b15043
denisiuk-aleksey <denisiuk.aleksei@gmail.com>
2021-03-19 22:17:25 +0200

    Add form and add style fot it

--- src/components/TodoInput/index.jsx
+++ src/components/TodoInput/index.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import { USER_TASK_SCHEMA } from '../../utils/validationSchems.js'
+import styles from './TodoInput.module.scss'
 
 const TodoInput = props => {
-  const [todoArray, setTodoArray] = useState([
-    {
-      id: Date.now(),
-      body: 'Your task may be here...',
-      isDone: true,
-      createdAt: new Date()
-    }
-  ])
-
-  const onSubmit = values => {
-    const newTask = {
-      body: values.userTask,
-      id: Date.now(),
-      isDone: false
-    }
-    setTodoArray([...todoArray, newTask])
-  }
+  const { onSubmit } = props
+  const initialValues = { todo: '' }
 
   return (
     <Formik
-      initialValues={{ todoBody: '' }}
-      onSubmit={onSubmit}
+      initialValues={initialValues}
       validationSchema={USER_TASK_SCHEMA}
+      onSubmit={(values, formikBag) => {
+        onSubmit(values)
+        formikBag.resetForm()
+      }}
     >
-      <Form>
-        <Field type='text' name='task' placeholder='Task...' />
-        <ErrorMessage component='span' name='task' />
+      <Form className={styles.container}>
+        <Field
+          type='text'
+          name='todo'
+          placeholder='Task...'
+          className={styles.input}
+        />
+        <ErrorMessage name='todo' component='span'  className={styles.errorMessage}/>
+
+        <button type='submit' className={styles.submitButton} onSubmit={onSubmit}>
+          Add Task
+        </button>
+
       </Form>
     </Formik>
   )
